d6c10ca3f6ec9962943d02d85d1552b3112614d0
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-09-15 15:03:33 -0300

    feat(auth): add login, JWT generation, and env validation=

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,42 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
+import { authConfig } from "@/configs/auth"
+import { AppError } from "@/utils/AppError"
+import { compare } from "bcrypt"
+import { prisma } from "@/database/prisma"
+import { z } from "zod"
+import { sign } from "jsonwebtoken"
 
 class SessionsController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      matricula: z.number(),
+      password: z.string().min(6),
+    })
+
+    const { matricula, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({
+      where: { matricula },
+    })
+
+    if (!user) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({ role: user.role ?? "collaborator" }, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ token })
   }
 }
 
